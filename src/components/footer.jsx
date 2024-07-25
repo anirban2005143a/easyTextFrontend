a7824f5a6550b8eb0d3c069dd323808c6578b88f
anirban2005143a <23je0104@iitism.ac.in>
2024-07-25 17:24:31 +0530

    fbtG

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,15 +11,15 @@ const footer = (props) => {
     const width = footer.clientWidth
 
     width >= 700 ? footerChildArr.forEach((child, index) => {
-      index !== 2 ? child.style.width = `${width * 0.333 - 50}px` : child.style.width = `${width * 0.2 + 50}px`
+      child.style.width = `${width / 3 - 10}px`
     }) : ''
 
-    550 < width && width <= 700 ? footerChildArr.forEach((child, index) => {
-      index !== 2 ? child.style.width = `${width * 0.5 - 50}px` : child.style.width = `${width * 1.0}px`
+    550 < width && width < 700 ? footerChildArr.forEach((child, index) => {
+      index === 2 ? child.style.width = `${width * 1.0 - 10}px` : child.style.width = `${width * 0.5 - 10}px`
     }) : ''
 
-    350 < width && width <= 550 ? footerChildArr.forEach((child, index) => {
-      child.style.width = `${width * 1 - 30}px`
+    350 <= width && width <= 550 ? footerChildArr.forEach((child, index) => {
+      child.style.width = `${width * 1 - 10}px`
     }) : ''
   }
 
@@ -34,36 +34,36 @@ const footer = (props) => {
 
   return (
     <div id='footer' ref={props.footerRef} className={`position-relative w-100 mb-4 ${props.isFooterVisible ? 'visibility' : ''}`} >
-      <div className="footer w-100 d-flex flex-wrap justify-content-around text-center">
+      <div className="footer w-100 d-flex flex-wrap ">
 
         <div className="services footerChild px-2 py-1 mt-3">
           <div className="ourServies" id="title"> All Services</div>
-          <div className="ourServies"><span> Blog </span></div>
-          <div className="ourServies"><span> Advertisment</span></div>
-          <div className="ourServies"><span> Email Writing</span></div>
-          <div className="ourServies"><span> Youtube Video Content</span></div>
-          <div className="ourServies"><span> Text Edit</span></div>
-          <div className="ourServies"><span> Summarize and Extend</span></div>
-          <div className="ourServies"><span> Analyze Images</span></div>
-          <div className="ourServies"><span> Analyze Audio</span></div>
-          <div className="ourServies"><span> Get and Execute Python</span></div>
-          <div className="ourServies"><span> Get json Object</span></div>
+          <div className="ourServies"><div> Blog </div></div>
+          <div className="ourServies"><div> Advertisment</div></div>
+          <div className="ourServies"><div> Email Writing</div></div>
+          <div className="ourServies"><div> Youtube Video Content</div></div>
+          <div className="ourServies"><div> Text Edit</div></div>
+          <div className="ourServies"><div> Summarize and Extend</div></div>
+          <div className="ourServies"><div> Analyze Images</div></div>
+          <div className="ourServies"><div> Analyze Audio</div></div>
+          <div className="ourServies"><div> Get and Execute Python</div></div>
+          <div className="ourServies"><div> Get json Object</div></div>
         </div>
 
         <div className="keyFeatures footerChild px-2 py-1 mt-3">
           <div className="keys" id="title"> Features</div>
-          <div className="keys"><span> Image Analyzation</span></div>
-          <div className="keys"><span> Audio Analyzation</span></div>
-          <div className="keys"><span> Code Execution</span></div>
-          <div className="keys"><span> Json Object</span></div>
+          <div className="keys"><div> Image Analyzation</div></div>
+          <div className="keys"><div> Audio Analyzation</div></div>
+          <div className="keys"><div> Code Execution</div></div>
+          <div className="keys"><div> Json Object</div></div>
         </div>
 
-        <div className="support footerChild px-2 py-1 mt-3 ">
+        <div className="support footerChild px-2 py-1 mt-3">
           <div className="supportItem" id="title"> Support</div>
-          <Link className=' text-decoration-none' to="/"> <div className="supportItem"><span> Home</span></div></Link>
-          <Link className=' text-decoration-none' to="/about"> <div className="supportItem"><span> About</span></div></Link>
-          <Link className=' text-decoration-none' to="/contact"> <div className="supportItem"><span> Contact</span></div></Link>
-          <Link className=' text-decoration-none' to="/requestFeature"> <div className="supportItem"><span> Request a new feature</span></div></Link>
+          <Link className=' text-decoration-none' to="/"> <div className="supportItem"><div> Home</div></div></Link>
+          <Link className=' text-decoration-none' to="/about"> <div className="supportItem"><div> About</div></div></Link>
+          <Link className=' text-decoration-none' to="/contact"> <div className="supportItem"><div> Contact</div></div></Link>
+          <Link className=' text-decoration-none' to="/requestFeature"> <div className="supportItem"><div> Request a new feature</div></div></Link>
         </div>
 
       </div>
